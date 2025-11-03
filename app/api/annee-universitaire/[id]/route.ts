c77c975e566b97c8aaaacdf8e06d6545b1fff851
Koneum <148181966+Koneum@users.noreply.github.com>
2025-11-03 21:34:53 +0000

    api

--- app/api/annee-universitaire/[id]/route.ts
+++ app/api/annee-universitaire/[id]/route.ts
@@ -51,7 +51,7 @@ export async function GET(
 
 export async function PUT(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const resolvedParams = await Promise.resolve(params);
