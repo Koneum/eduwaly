c10608379b519a2be2daeeaca7cf244e16af7229
Koneum <148181966+Koneum@users.noreply.github.com>
2025-11-05 23:56:41 +0000

    correction vercel error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { 
     prisma: PrismaClient
-}
+} 
 
 const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
